fea5bc692eff3707f066fcdc6a0a516551cf1e47
Cole Townsend <cole@coletownsend.com>
2013-05-14 22:20:16 -0400

    Scripts, styles, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,19 @@ $(function(){
         setTimeout(function(){ t.addClass('slider'); }, (i+1) * 330);
     });
 });
+
+(function($, self){
+
+if(!$ || !self) {
+	return;
+}
+
+for(var i=0; i<self.properties.length; i++) {
+	var property = self.properties[i],
+		camelCased = StyleFix.camelCase(property),
+		PrefixCamelCased = self.prefixProperty(property, true);
+	
+	$.cssProps[camelCased] = PrefixCamelCased;
+}
+
+})(window.jQuery, window.PrefixFree);
